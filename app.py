feb4e5b06cf8073f1a6fb0d2028ce5626d080ce1
JohnySmoke <35803428+JohnySmoke@users.noreply.github.com>
2024-06-21 19:23:13 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def send_message(chat_id, text, button_text, button_url):
     requests.post(url, json=payload)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
