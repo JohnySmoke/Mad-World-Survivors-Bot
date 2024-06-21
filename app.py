922f4f3b0459b64dcf01ebfb433b12c1e467f84d
JohnySmoke <35803428+JohnySmoke@users.noreply.github.com>
2024-06-21 20:12:32 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,25 +7,23 @@ app = Flask(__name__)
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN', '7383030976:AAF49Fo_8ZpMORFnQqapGtOH-BSKCM7Izyg')
 TELEGRAM_API_URL = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/'
 
+@app.route('/')
+def index():
+    return 'Hello, this is the root URL of the Flask app.'
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
     if 'message' in data:
         chat_id = data['message']['chat']['id']
-        send_message(chat_id, 'Нажмите на кнопку ниже, чтобы запустить игру', 'Запустить игру', 'https://itch.io/embed-upload/10665163?color=000000')
+        send_inline_button(chat_id, 'Запустить игру', 'https://itch.io/embed-upload/10665163?color=000000')
     return 'ok'
 
-def send_message(chat_id, text, button_text, button_url):
-    url = TELEGRAM_API_URL + 'sendMessage'
+def send_inline_button(chat_id, text, url):
+    url_button = f"url={url}"
     payload = {
-        'chat_id': chat_id,
-        'text': text,
-        'reply_markup': {
-            'inline_keyboard': [[{'text': button_text, 'url': button_url}]]
-        }
+        'inline_keyboard': [
+            [{'text': text, 'url_button': url_button}]
+        ]
     }
-    requests.post(url, json=payload)
-
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
+    requests.post(TELEGRAM_API_URL, json=payload)
